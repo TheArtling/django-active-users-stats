1e47198e151317d5e0bc98e8f70ec33e810b7414
Nicholas Ng <nzy.nicholas@gmail.com>
2017-02-28 18:19:02 +0800

    Minor fixes to util functions

--- active_users/utils.py
+++ active_users/utils.py
@@ -8,7 +8,15 @@ def user_in_blacklist(username):
 
 def urls_in_blacklist(url):
     for pattern in settings.urls_blacklist():
+        if pattern == '':
+            continue
         compiled_pattern = re.compile(pattern)
         if compiled_pattern.match(url):
             return True
     return False
+
+
+def is_blacklisted(request):
+    return (request.user.is_superuser or
+            user_in_blacklist(request.user.username) or
+            urls_in_blacklist(request.path))
