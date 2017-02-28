9dc8822c47e933c43714087515a63b4a54a8e7ca
Nicholas Ng <nzy.nicholas@gmail.com>
2017-02-28 19:02:45 +0800

    Cleaned up middleware

--- active_users/middleware.py
+++ active_users/middleware.py
@@ -1,6 +1,4 @@
 """Middleware classes for the active_users app."""
-from django.utils.timezone import now
-
 from .utils import is_blacklisted
 from .models import Activity
 
@@ -21,12 +19,4 @@ class ActiveUsersMiddleware(object):
 
     def process_request(self, request):
         if request.user.is_authenticated() and not is_blacklisted(request):
-            day = now().date()
-            activity, created = Activity.objects.get_or_create(
-                user=request.user,
-                day=day,
-                defaults={'day': day}
-            )
-            if not created:
-                activity.count += 1
-                activity.save()
+            Activity.objects.increment_now(user=request.user)
