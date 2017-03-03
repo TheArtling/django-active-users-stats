0f9e9babf43f73bc57e858618649811e44ce89f3
Nicholas Ng <nzy.nicholas@gmail.com>
2017-03-03 10:24:46 +0800

    Added DAU, MAU, Stickiness queries

--- active_users/queries.py
+++ active_users/queries.py
@@ -1,9 +1,79 @@
 """Queries for the active_users app."""
 from django.contrib.auth.models import User
+from decimal import Decimal, localcontext
 
 from dateutil.relativedelta import relativedelta
 
-from query_utils import get_months_range, parse_date
+from .models import Activity
+from query_utils import (
+    get_months_range, parse_date, safe_div, stagger_tuple,
+    get_first_last_day_of_month
+)
+
+
+def get_dau_for_period(start_date, end_date):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    date_diff = end_date - start_date
+    day_qs = Activity.objects.all()
+    day_qs = day_qs.filter(day__range=(start_date, end_date))
+    with localcontext() as ctx:
+        ctx.prec = 4
+        average = safe_div(Decimal(day_qs.count()), date_diff.days+1)
+    return average
+
+
+def get_mau_for_period(start_date, end_date):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    month_qs = Activity.objects.all()
+    month_qs = month_qs.filter(day__range=(start_date, end_date))
+    month_qs = month_qs.values_list('user__id', flat=True).distinct()
+    return month_qs.count()
+
+
+def get_stickiness_for_period(start_date, end_date):
+    dau = get_dau_for_period(start_date, end_date)
+    mau = get_mau_for_period(start_date, end_date)
+    with localcontext() as ctx:
+        ctx.prec = 4
+        result = safe_div(dau, mau, default=0)
+    return result
+
+
+def get_mau_per_month(start_date, end_date):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    months = get_months_range(start_date, end_date)
+    month_tuple = [
+        get_first_last_day_of_month(month.year, month.month)
+        for month in months
+    ]
+    return [get_mau_for_period(start, end) for start, end in month_tuple]
+
+
+def get_dau_per_month(start_date, end_date):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    months = get_months_range(start_date, end_date)
+    month_tuple = [
+        get_first_last_day_of_month(month.year, month.month)
+        for month in months
+    ]
+    return [get_dau_for_period(start, end) for start, end in month_tuple]
+
+
+def get_stickiness_per_month(start_date, end_date, keep_day=False):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    months = get_months_range(start_date, end_date, keep_day=keep_day)
+    month_tuple = [
+        get_first_last_day_of_month(month.year, month.month)
+        for month in months
+    ]
+    return [
+        get_stickiness_for_period(start, end) for start, end in month_tuple
+    ]
 
 
 def get_retained_users_per_month(start_date, end_date):
@@ -24,12 +94,7 @@ def get_retained_users_per_month(start_date, end_date):
     months = get_months_range(start_date, end_date)
 
     previous_month = start_date - relativedelta(months=1)
-    months_formatted = []
-    for month in months:
-        months_formatted.append(
-            (previous_month, month)
-        )
-        previous_month = month
+    months_formatted = stagger_tuple(months, previous_month)
 
     results = []
     for month_tuple in months_formatted:
