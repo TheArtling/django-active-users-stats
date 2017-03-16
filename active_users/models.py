2a2c7c72194fc838501a22d6c18c714f72f9c044
Nicholas Ng <nzy.nicholas@gmail.com>
2017-03-16 12:50:16 +0800

    Added unqiue_together constraint and last_active field

--- active_users/models.py
+++ active_users/models.py
@@ -29,8 +29,12 @@ class ActivityManager(models.Manager):
 
 class Activity(models.Model):
     day = models.DateField()
+    last_active = models.DateTimeField(auto_now=True)
     count = models.IntegerField(default=1)
     user = models.ForeignKey(
         'auth.User', related_name='activity')
 
     objects = ActivityManager()
+
+    class Meta:
+        unique_together = ("day", "user")
