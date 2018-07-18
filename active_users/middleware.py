ed1090ebf544a58fd5efaa9232f563d9f9f26e2e
PohPengYu <pohpengyu@gmail.com>
2018-07-18 14:17:18 +0800

    Django 2.0 compatibility (is_authtenticated attribute instead of fn)

--- active_users/middleware.py
+++ active_users/middleware.py
@@ -8,5 +8,5 @@ from .models import Activity
 class ActiveUsersMiddleware(MiddlewareMixin):
     """Tracks activity of users (count of actions per day)."""
     def process_request(self, request):
-        if request.user.is_authenticated() and not is_blacklisted(request):
+        if request.user.is_authenticated and not is_blacklisted(request):
             Activity.objects.increment_now(user=request.user)
